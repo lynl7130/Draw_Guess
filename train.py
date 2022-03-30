b563b82e8502b19f69bdb8a4fc8d076f01510583
lynl7130 <lynl7130@gmail.com>
2022-03-30 19:48:54 -0400

    checkpoint done

--- train.py
+++ train.py
@@ -2,9 +2,10 @@ import runners
 import data
 import torch
 from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import LearningRateMonitor, TQDMProgressBar
+from pytorch_lightning.callbacks import LearningRateMonitor, TQDMProgressBar, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 from configs import get_args
+import os
 
 config = get_args()
 train_ratio = float(config["train_base"]-1) / float(config["train_base"])
@@ -29,15 +30,28 @@ model = runners.Classifier(lr=config["lr"],
     steps_per_epoch = steps_per_epoch)
 model.datamodule = datamodule
 
+# tensorboard logger will automatically create a version directory for us under log_dir/exp_name 
+# need do this ahead because ModelCheckpoint rely on logger to decide checkpoing save dir
+logger = TensorBoardLogger(config["log_dir"], name=config["exp_name"])
 
 trainer = Trainer(
     max_epochs = config["max_epochs"],
     gpus=min(1, torch.cuda.device_count()),
-    logger=TensorBoardLogger(config["log_dir"], name=config["exp_name"]),
+    logger=logger,
     callbacks=[LearningRateMonitor(logging_interval="step"),
-        TQDMProgressBar(refresh_rate=config["refresh_rate"])
+        TQDMProgressBar(refresh_rate=config["refresh_rate"]),
+        ModelCheckpoint(
+            dirpath=os.path.join(logger.log_dir, "checkpoints"),
+            filename="{epoch}-{step}-{val_loss:.2f}",
+            monitor="val_loss",
+            save_last=True,
+            save_top_k=2,
+            mode="min",
+            save_on_train_epoch_end=True
+            )
     ],
     val_check_interval=config["val_check_interval"]
 )
-trainer.fit(model, model.datamodule)
+# even when starting from an existing checkpoint path, will create a new version directory
+trainer.fit(model, model.datamodule, ckpt_path=config["resume_path"] if config["resume_path"] != "" else None)
 trainer.test(model, model.datamodule)
